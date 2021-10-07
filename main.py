4703838c5d4c315bfb78fa93b6eb21d1d8d7d299
user0481 <egisxxegis@gmail.com>
2021-10-07 21:22:42 +0300

    typo. codone -> Codon; dicodone -> dicodon

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     mamalians = [f'sources\\data\\mamalian{x+1}.fasta' for x in range(4)]
 
     all_viruses = bacterials + mamalians
-    all_viruses_codones_frequencies = []
-    all_viruses_dicodones_frequencies = []
+    all_viruses_codons_frequencies = []
+    all_viruses_dicodons_frequencies = []
 
     for one_file_name in all_viruses:
         for seq_record in SeqIO.parse(one_file_name, "fasta"):
@@ -40,18 +40,18 @@ if __name__ == '__main__':
             # task 3 done above.
 
             translated_nucleotides = "ARNDCEQGHILKMFPSTWYV"
-            translated_dicodones = [codone + codone2
-                                    for codone in translated_nucleotides
-                                    for codone2 in translated_nucleotides]
-            frequency_of_codones = count_frequency(all_frames_translated, translated_nucleotides)
-            frequency_of_dicodones = count_frequency(all_frames_translated, translated_dicodones)
+            translated_dicodons = [codon + codon2
+                                   for codon in translated_nucleotides
+                                   for codon2 in translated_nucleotides]
+            frequency_of_codons = count_frequency(all_frames_translated, translated_nucleotides)
+            frequency_of_dicodons = count_frequency(all_frames_translated, translated_dicodons)
             print(f'\nthe table of codon frequencies in {seq_record.id}\n')
-            print_data_in_table(frequency_of_codones.tolist(),
+            print_data_in_table(frequency_of_codons.tolist(),
                                 top_down_headers=translated_nucleotides,
-                                left_right_headers=["Codone", "Frequency"],
+                                left_right_headers=["codon", "Frequency"],
                                 dimensions=2)
             print(f'\nthe table of dicodon frequencies in {seq_record.id}\n')
-            print_data_in_table(format_numpy_array_digits(frequency_of_dicodones,
+            print_data_in_table(format_numpy_array_digits(frequency_of_dicodons,
                                                           decimal_spaces=4,
                                                           should_near_zero_be_adjusted_to_low_value=True,
                                                           do_the_round=True).tolist(),
@@ -60,34 +60,34 @@ if __name__ == '__main__':
                                 dimensions=2,)
             # task 4 done above.
 
-            all_viruses_codones_frequencies.append((seq_record.id, frequency_of_codones))
-            all_viruses_dicodones_frequencies.append((seq_record.id, frequency_of_dicodones))
+            all_viruses_codons_frequencies.append((seq_record.id, frequency_of_codons))
+            all_viruses_dicodons_frequencies.append((seq_record.id, frequency_of_dicodons))
 
     # end for - all records have been read
     # now it is time to compare
-    distance_codone_in_phylip = [len(all_viruses_codones_frequencies)]
-    for frequency in all_viruses_codones_frequencies:
+    distance_codon_in_phylip = [len(all_viruses_codons_frequencies)]
+    for frequency in all_viruses_codons_frequencies:
         frequency_row = [frequency[0]]  # name of sample
-        for i in range(len(all_viruses_codones_frequencies)):
+        for i in range(len(all_viruses_codons_frequencies)):
             distance = calculate_difference_frequencies(frequency[1],
-                                                        all_viruses_codones_frequencies[i][1])
+                                                        all_viruses_codons_frequencies[i][1])
             frequency_row.append(f'{distance:2.5f}')
-        distance_codone_in_phylip.append(frequency_row)
+        distance_codon_in_phylip.append(frequency_row)
 
-    distance_dicodone_in_phylip = [len(all_viruses_dicodones_frequencies)]
-    for frequency in all_viruses_dicodones_frequencies:
+    distance_dicodon_in_phylip = [len(all_viruses_dicodons_frequencies)]
+    for frequency in all_viruses_dicodons_frequencies:
         frequency_row = [frequency[0]]  # name of sample
-        for i in range(len(all_viruses_dicodones_frequencies)):
+        for i in range(len(all_viruses_dicodons_frequencies)):
             distance = calculate_difference_frequencies(frequency[1],
-                                                        all_viruses_dicodones_frequencies[i][1])
+                                                        all_viruses_dicodons_frequencies[i][1])
             frequency_row.append(f'{distance:2.5f}')
-        distance_dicodone_in_phylip.append(frequency_row)
+        distance_dicodon_in_phylip.append(frequency_row)
 
     print("\nCodon distance matrix in Phylip")
-    print_in_phylip(distance_codone_in_phylip)
+    print_in_phylip(distance_codon_in_phylip)
 
     print("\nDicodon distance matrix in Phylip")
-    print_in_phylip(distance_dicodone_in_phylip)
+    print_in_phylip(distance_dicodon_in_phylip)
     # task 5 done above.
 
 else:
